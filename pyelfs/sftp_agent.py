7b793e271f07a127c7711366ab3333660360e45b
Yusuke Kawashima <y-kawashima@hoshi.ac.jp>
2020-07-25 04:50:18 +0900

    fix temp

--- pyelfs/sftp_agent.py
+++ pyelfs/sftp_agent.py
@@ -5,6 +5,7 @@ from logging import getLogger
 from os.path import expanduser
 from random import random
 from time import sleep
+from tempfile import gettempdir
 
 from . import CustomTransferAgent
 from .sftp_auth import SftpAuth
@@ -119,7 +120,9 @@ class SftpAgent(CustomTransferAgent):
                                  "please add 'pyelfs://' in .git/config, "
                                  "in order to avoid unintentional path expansion by git-lfs. ")
         parser.add_argument("--verbose", help="verbose log")
-        parser.add_argument("--temp", help="temporary directory to download lfs objects.")
+        parser.add_argument("--temp",
+                            default=f"{gettempdir()}",
+                            help="temporary directory to download lfs objects.")
 
     @classmethod
     def rep(cls):
